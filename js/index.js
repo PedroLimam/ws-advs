f74db2460278c3f3b4af1d13134fd88f48f09d92
Pedro Lima <pedrolimir@gmail.com>
2024-01-04 10:22:33 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,37 @@
+const $btnsAccordion = document.querySelectorAll(".btn-accordion");
+const $spanAccordion = document.querySelectorAll(".span-accordion")
+const $itemsAccordions = document.querySelectorAll(".section-sobre-nos__card-adv__infos__list-historico")
+
+$btnsAccordion.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        const orderAccordion = e.target.getAttribute("accordion");
+
+        $itemsAccordions[orderAccordion-1].classList.toggle("section-sobre-nos__card-adv__infos__list-historico--open");
+        $btnsAccordion[orderAccordion-1].innerHTML = "Ler mais"
+
+        console.log($spanAccordion[orderAccordion-1])
+
+
+        if ($itemsAccordions[orderAccordion-1].classList.contains("section-sobre-nos__card-adv__infos__list-historico--open")) {
+            $btnsAccordion[orderAccordion-1].innerHTML = `Ler menos <span class="span-accordion span-open" accordion=${orderAccordion}>
+            &rsaquo;</span>`
+        } else{
+            $btnsAccordion[orderAccordion-1].innerHTML = `Ler mais <span class="span-accordion" accordion=${orderAccordion}>
+            &rsaquo;
+        </span>`
+        }
+    })
+})
+
+const $formEmail = document.querySelector("#formEmail")
+$formEmail.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const $name = document.querySelector("#name")
+    const $email = document.querySelector("#email")
+    const $textArea = document.querySelector("#textarea")
+
+    console.log($name.value, $email.value, $textArea.value)
+    
+  });
+
